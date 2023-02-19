b26054d8ba734c778c8492c7c751367764fdfbaf
Indrarjun <billasaketh@gmail.com>
2023-02-19 23:35:31 +0530

    Completed Submit form

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express= require('express');
 const expressLayouts = require('express-ejs-layouts');
+const fileupload = require('express-fileupload');
+const session = require('express-session');
+const cookieParser= require('cookie-parser');
+const flash = require('connect-flash');
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +14,16 @@ require('dotenv').config();
 app.use(express.urlencoded( { extended:true}));
 app.use(express.static('public'));
 app.use(expressLayouts);
+
+app.use(cookieParser('CookingBlogSecure'));
+app.use(session({
+    secret: 'CookingBlogSecretSession',
+    saveUninitialized:true,
+    resave: true,
+}));
+app.use(flash());
+app.use(fileupload());
+
 
 app.set('layout','./layouts/main');
 app.set('view engine', 'ejs')
